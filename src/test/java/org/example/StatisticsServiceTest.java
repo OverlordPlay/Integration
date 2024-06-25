1c4ba379db29fc238be13cea6cf26ba8272b0f33
Learn <egivmir59@gmail.com>
2024-06-25 13:12:39 +0200

    PomAdd

--- src/test/java/org/example/StatisticsServiceTest.java
+++ src/test/java/org/example/StatisticsServiceTest.java
@@ -1,4 +1,4 @@
-package org.example.;
+package org.example;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +11,18 @@ public class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfEqual() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
